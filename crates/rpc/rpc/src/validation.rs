b92e1f9dcc4ddf91a4748003af1300a2882e430a
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2026-02-27 20:41:17 +0100

    fix: missing filter check

--- crates/rpc/rpc/src/validation.rs
+++ crates/rpc/rpc/src/validation.rs
@@ -136,7 +136,7 @@ where
         self.consensus.validate_header(block.sealed_header())?;
         self.consensus.validate_block_pre_execution(block.sealed_block())?;
 
-        if !self.disallow.is_empty() && transaction_filter != TransactionFilter::None {
+        if !self.disallow.is_empty() && transaction_filter == TransactionFilter::OFAC {
             if self.disallow.contains(&block.beneficiary()) {
                 return Err(ValidationApiError::Blacklist(block.beneficiary()))
             }
@@ -212,7 +212,7 @@ where
                 executor.take_bal().map(|bal| compute_block_access_list_hash(&bal));
 
             let mut state = executor.into_state();
-            if !self.disallow.is_empty() {
+            if !self.disallow.is_empty() && transaction_filter == TransactionFilter::OFAC {
                 // Check whether the submission interacted with any blacklisted account by
                 // scanning the `State`'s cache that records everything read from database
                 // during execution.
